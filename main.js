afacb15786bf5bf8a7fd890c24f7ebc9ee44da02
SRPVT <habibi786123r@gmail.com>
2024-05-30 14:07:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById('disableAnim').onclick = function() {
   const body = document.getElementById('about');
   body.classList.toggle('disableAnimation');
   console.log(this.innerText);
-  if (this.innerText === 'Disable Animations') {
+  if (this.innerText === 'Enable Animations') {
     this.innerText = 'Disable Animations';
   } else {
     this.innerText = 'Enable Animations'
