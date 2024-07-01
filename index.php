94824e87d5cf11200ed11b47dcd35bee113175d4
SRPVT <habibi786123r@gmail.com>
2024-07-01 20:45:14 +0530

    Update and rename index,html to index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <meta name="author" content="Amardeep Kesharwani" >
-<title>Skibidi quiz</title>
+<title>Skibidi quiz beta</title>
 <link href="https://fonts.googleapis.com/css?family=Julius+Sans+One|Google Sans" rel="stylesheet">
 </head>
 <body>
